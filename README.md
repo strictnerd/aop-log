3a5f46fa2ad72ebbd2c1ad59d42af6d8c0742048
clq <867359230@qq.com>
2018-01-13 19:59:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# web-mvc
+
+---
+
+集成了nutz+durid+mysql+基于注解的日志
+
+
+## 使用说明
+1. git clone https://github.com/strictnerd/web-mvc.git
+2. maven clean install
+3. maven jetty:run
+4. 测试访问：http://127.0.0.1:2017/clq-web-mvc/admin/public/test
+
---
+++ src/main/java/com/log/aop/MethodAspect.java
@@ -0,0 +1,65 @@
+package com.log.aop;
+
+import com.alibaba.fastjson.JSON;
+import com.log.annotation.LogMethod;
+import com.log.bean.ParameterBean;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+
+/**
+ * 记录@LogMethod注解方法的调用日志
+ * @author Chen
+ * @version 1.0.0
+ */
+@Slf4j
+@Aspect
+public class MethodAspect {
+
+    private static final Log log = LogFactory.getLog(MethodAspect.class);
+
+    @Around("execution(* *.*(..)) && @annotation(com.log.annotation.LogMethod)")
+    public Object profile(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+
+        ParameterBean parameterBean = new ParameterBean();
+        //方法名
+        String target = joinPoint.getSignature().getName();
+        parameterBean.setMethod(target);
+        if(joinPoint.getSignature() instanceof MethodSignature){
+            MethodSignature signature = (MethodSignature)joinPoint.getSignature();
+            LogMethod logMethod = signature.getMethod().getAnnotation(LogMethod.class);
+            if (logMethod != null && logMethod.value() != null) {
+                //输出方法值
+                target = logMethod.value();
+            }
+        }
+        //参数
+        try {
+            if (joinPoint.getArgs() != null) {
+                parameterBean.setParams(joinPoint.getArgs());
+            }
+        } catch (Exception e) {
+            log.info("序列化方法参数异常，无法输出日志");
+        }
+        Object result = null;
+        try {
+            //执行
+            result = joinPoint.proceed();
+            if (result != null) {
+                parameterBean.setRetValue(result);
+            }
+            parameterBean.setTime((System.currentTimeMillis()-startTime));
+        } catch (Exception e) {
+            log.info("序列化响应异常,无法输出日志");
+        }finally {
+            log.info(JSON.toJSONString(parameterBean));
+        }
+        return result;
+
+    }
+}
